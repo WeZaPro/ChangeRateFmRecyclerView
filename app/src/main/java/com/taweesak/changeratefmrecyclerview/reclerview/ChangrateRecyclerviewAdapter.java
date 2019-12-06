23e035fa71c5b945cec1c4b1e35bbd99a25743b0
taweesak <taweesak9359@gmail.com>
2019-12-06 15:58:24 +0700

    update3

--- app/src/main/java/com/taweesak/changeratefmrecyclerview/reclerview/ChangrateRecyclerviewAdapter.java
+++ app/src/main/java/com/taweesak/changeratefmrecyclerview/reclerview/ChangrateRecyclerviewAdapter.java
@@ -1,6 +1,70 @@
 package com.taweesak.changeratefmrecyclerview.reclerview;
 
-public class ChangrateRecyclerviewAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.taweesak.changeratefmrecyclerview.R;
+import com.taweesak.changeratefmrecyclerview.dataModel.Model;
 
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChangrateRecyclerviewAdapter extends RecyclerView.Adapter<ChangrateRecyclerviewAdapter.VH> {
+
+    private Context context;
+    private ArrayList<Model> mDataList;
+
+    public ChangrateRecyclerviewAdapter(Context context, ArrayList<Model> mDataList) {
+        this.context = context;
+        this.mDataList = mDataList;
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_item, parent, false);
+
+        return new VH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH holder, int position) {
+
+        holder._name.setText(mDataList.get(position).getText());
+        holder.itemView.setTag(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    public class VH extends RecyclerView.ViewHolder {
+
+        TextView _name;
+
+        public VH(View itemView) {
+            super(itemView);
+
+            _name = itemView.findViewById(R.id.tv_name);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int clickItem = (int) view.getTag();
+                    Toast.makeText(context,"item number : "+clickItem,Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
 }
--- app/src/main/java/com/taweesak/changeratefmrecyclerview/ui/RvFragment.java
+++ app/src/main/java/com/taweesak/changeratefmrecyclerview/ui/RvFragment.java
@@ -1,10 +1,13 @@
 package com.taweesak.changeratefmrecyclerview.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +15,16 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.taweesak.changeratefmrecyclerview.R;
+import com.taweesak.changeratefmrecyclerview.dataModel.Model;
+import com.taweesak.changeratefmrecyclerview.reclerview.ChangrateRecyclerviewAdapter;
 
 import java.util.ArrayList;
 
 public class RvFragment extends Fragment {
+
+    private Context context;
+    private ArrayList<Model> lstData = new ArrayList<>();
+    RecyclerView recyclerView;
 
     public RvFragment() {
         // Required empty public constructor
@@ -32,6 +41,13 @@ public class RvFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rv, container, false);
+
+        data();
+
+        recyclerView = view.findViewById(R.id.myRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        ChangrateRecyclerviewAdapter myAdapter = new ChangrateRecyclerviewAdapter(getActivity(),lstData);
+        recyclerView.setAdapter(myAdapter);
         return view;
     }
 
@@ -39,6 +55,17 @@ public class RvFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    private void data() {
+        String name_[] = {"Kitty 1","Kitty 2","Kitty 3","Kitty 4","Kitty 5","Kitty 6","Kitty 7"};
+
+        int dataSize = name_.length;
+
+        for (int i = 0; i <dataSize ; i++) {
+            Model model = new Model(name_[i]);
+            lstData.add(model);
+        }
     }
 
 }
